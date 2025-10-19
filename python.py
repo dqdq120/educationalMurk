c550399a6ceb4dc68173ac9aa651dfe9abdd89e5
mohamed ammar <dqdq120@outlook.com>
2025-10-19 16:57:23 +0200

    fixing the word courses

--- python.py
+++ python.py
@@ -2,10 +2,10 @@ from wordfreq import top_n_list
 import json
 
 # Get top 5000 English words
-top5000 = top_n_list("en", 5000)
+top5000 = top_n_list("en", 40000)
 
 # Save to JSON
-with open("top5000_words.json", "w", encoding="utf8") as f:
+with open("top40000_words.json", "w", encoding="utf8") as f:
     json.dump(top5000, f, ensure_ascii=False, indent=2)
 
-print("Saved to top5000_words.json")
+print("Saved to top40000_words.json")
